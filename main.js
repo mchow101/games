9c24e87bf6696934788ae88787e3983fc933ab49
M. Chowdhury <mintychocolate101@gmail.com>
2017-06-26 14:17:46 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,36 +1,90 @@
 $(document).ready(function() {
 	'use strict';
-	paper.install(window);
-	paper.setup(document.getElementById('myCanvas'));
+	var cheight = 400;
+	var cwidth = 400;
+	var dicex = 50;
+	var dicey = 50;
+	var dicewidth = 100;
+	var diceheight = 100;
+	var dotrad = 6;
+	var ctx;
+	console.log('test');
 
-	var c = Shape.Circle(400, 400, 80);
-	c.fillColor = 'black';
-	var text = new PointText(400, 400);
-	text.justification = 'center';
-	text.fillColor = 'white';
-	text.fontSize = 20;
-	text.content = 'Hello World!';
+	function init() {
+		console.log('init');
+		var ch = 1 + Math.floor(Math.random()*6);
+		drawface(ch);
+	}
 
-	var tool = new Tool();
-	tool.onMouseDown = function(event) {
-		var c = Shape.Circle(event.point, 20);
-		if(event.point.x > 400) {
-			if(event.point.y > 400) {
-				c.fillColor = 'pink';
-			}
-			else {
-				c.fillColor = 'red';
-			}
-		}
-		else {
-			if(event.point.y > 400) {
-				c.fillColor = 'blue';
-			}
-			else {
-				c.fillColor = 'orange';
-			}
+	function drawface(n) {
+		console.log('drawface worked');
+		ctx = document.getElementById('canvas').getContext('2d');
+		ctx.lineWidth = 5;
+		ctx.clearRect(dicex, dicey, dicewidth, diceheight);
+		ctx.strokeRect(dicex, dicey, dicewidth, diceheight);
+		ctx.fillStyle = "#009966";
+		switch(n) {
+			case 1: draw1(); break;
+			case 2: draw2(); break;
+			case 3: draw2(); draw1(); break;
+			case 4: draw4(); break;
+			case 5: draw4(); draw1(); break;
+			case 6: draw4(); draw2mid(); break;
 		}
 	}
-	paper.view.draw();
+
+	function draw1() {
+		console.log('draw1 function');
+		var dotx =  dicex + .5*dicewidth; 
+		var doty = dicey + .5*diceheight;
+		ctx.beginPath();
+		ctx.arc(dotx, doty, dotrad, 0, Math.PI*2, true);
+		ctx.closePath();
+		ctx.fill();
+	}
+
+	function draw2() {
+		var dotx =  dicex + 3*dotrad; 
+		var doty = dicey + 3*dotrad;
+		ctx.beginPath();
+		ctx.arc(dotx, doty, dotrad, 0, Math.PI*2, true);
+		dotx = dicex + dicewidth - 3*dotrad;
+		doty = dicey + diceheight - 3*dotrad;
+		ctx.arc(dotx, doty, dotrad, 0, Math.PI*2, true);
+		ctx.closePath();
+		ctx.fill();
+	}
+
+	function draw4() {
+		var dotx =  dicex + 3*dotrad; 
+		var doty = dicey + 3*dotrad;
+		ctx.beginPath();
+		ctx.arc(dotx, doty, dotrad, 0, Math.PI*2, true);
+		dotx = dicex + dicewidth - 3*dotrad;
+		doty = dicey + diceheight - 3*dotrad;
+		ctx.arc(dotx, doty, dotrad, 0, Math.PI*2, true);
+		ctx.closePath();
+		ctx.fill();
+		ctx.beginPath();
+		dotx =  dicex + dicewidth - 3*dotrad; 
+		doty = dicey + 3*dotrad;
+		ctx.arc(dotx, doty, dotrad, 0, Math.PI*2, true);
+		dotx = dicex + 3*dotrad;
+		doty = dicey + diceheight - 3*dotrad;
+		ctx.arc(dotx, doty, dotrad, 0, Math.PI*2, true);
+		ctx.closePath();
+		ctx.fill();
+	}
+
+	function draw2mid() {
+		var dotx = dicex + 3*dotrad;
+		var doty = dicey + .5*diceheight;
+		ctx.beginPath();
+		ctx.arc(dotx, doty, dotrad, 0, Math.PI*2, true);
+		dotx = dicex + dicewidth - 3*dotrad;
+		ctx.arc(dotx, doty, dotrad, 0, Math.PI*2, true);
+		ctx.closePath();
+		ctx.fill();
+	}
 	console.log('file loaded');
 });
